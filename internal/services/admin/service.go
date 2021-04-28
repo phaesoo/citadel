5ffb2c9eab7e5a8772928ba85b1f4389f21588bb
Haesoo Park <phaesoo@gmail.com>
2021-04-28 19:30:42 +0900

    Implementing on service

--- internal/services/admin/service.go
+++ internal/services/admin/service.go
@@ -1,25 +1,68 @@
 package admin
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
+	"context"
 
 	"github.com/google/uuid"
+	"github.com/phaesoo/keybox/internal/models"
+	"github.com/phaesoo/keybox/pkg/pem"
+	"github.com/pkg/errors"
 )
 
+type repo interface {
+	AuthKey(ctx context.Context, userID, keyID string) (models.AuthKey, error)
+	SetAuthKey(ctx context.Context, authKey models.AuthKey) error
+	DeleteAuthKey(ctx context.Context, userID, keyID string) error
+}
+
 type Service struct {
+	repo repo
 }
 
-func NewService() *Service {
-	return &Service{}
+func NewService(repo repo) *Service {
+	return &Service{repo: repo}
 }
 
-func (s *Service) RegisterKey() (string, error) {
-	keyID := uuid.NewString()
-	_, err := rsa.GenerateKey(rand.Reader, 2048)
+func (s *Service) RegisterKey(ctx context.Context, userID string) (string, error) {
+	privateKey, publicKey := pem.GenerateRsaKeyPair(2048)
+
+	pubicPem, err := pem.ExportRsaPublicKeyAsPemStr(publicKey)
 	if err != nil {
+		return "", errors.Wrap(err, "Export RSA public key")
+	}
+
+	authKey := models.AuthKey{
+		KeyID:      uuid.NewString(),
+		PublicKey:  pubicPem,
+		PrivateKey: pem.ExportRsaPrivateKeyAsPemStr(privateKey),
+		UserID:     userID,
+	}
+
+	if err := s.repo.SetAuthKey(ctx, authKey); err != nil {
 		return "", err
 	}
 
-	return keyID, nil
+	return authKey.KeyID, nil
+}
+
+func (s *Service) DeregisterKey(ctx context.Context, userID string) (string, error) {
+	privateKey, publicKey := pem.GenerateRsaKeyPair(2048)
+
+	pubicPem, err := pem.ExportRsaPublicKeyAsPemStr(publicKey)
+	if err != nil {
+		return "", errors.Wrap(err, "Export RSA public key")
+	}
+
+	authKey := models.AuthKey{
+		KeyID:      uuid.NewString(),
+		PublicKey:  pubicPem,
+		PrivateKey: pem.ExportRsaPrivateKeyAsPemStr(privateKey),
+		UserID:     userID,
+	}
+
+	if err := s.repo.SetAuthKey(ctx, authKey); err != nil {
+		return "", err
+	}
+
+	return authKey.KeyID, nil
 }
